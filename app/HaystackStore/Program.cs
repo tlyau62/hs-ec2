f4652e22603eb852e89965556301f84149eb1755
Tim Yau <yaurfu@gmail.com>
2025-04-10 10:40:24 +0800

    Add swagger

--- app/HaystackStore/Program.cs
+++ app/HaystackStore/Program.cs
@@ -6,6 +6,17 @@ builder.Services.AddSingleton<IVolumeManger, VolumeManger>();
 builder.Services.AddSingleton<IStoreService, StoreService>();
 builder.Services.AddSingleton<INeedleCache>(sp => new NeedleCache(1));
 
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
