49cfe59312194de9a2ef0b1216e3bcbbe64ad4ee
Tim Yau <yaurfu@gmail.com>
2025-04-12 14:08:43 +0800

    Add "LoadPhotos"

--- app/HaystackStore/Store/IStoreService.cs
+++ app/HaystackStore/Store/IStoreService.cs
@@ -10,5 +10,12 @@ public interface IStoreService
      * The filename of a photo is used as the key extracted by regex keyPattern.
      */
     void UnpackPhotos(string keyPattern, byte[] data);
+
+    /**
+     * Same as UnpackPhotos, but the zip is loaded from the existing location in the system.
+     * This is for loading a photo zip uploaded directly from EBS
+     * Location is relative to the mountFolder
+     */
+    void LoadPhotos(string keyPattern, string location);
 }
 
